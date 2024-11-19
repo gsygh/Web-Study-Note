33a22c6b4b107dd60e569fd37d3965b4388d2002
gsygh <gsygh@naver.com>
2024-11-19 22:39:33 +0900

    ~extension

--- react_example/begin-react/src/state_example/InputSample.js
+++ react_example/begin-react/src/state_example/InputSample.js
@@ -1,25 +1,25 @@
-import React, {useState} from "react";
-
+import React, { useState } from 'react';
 
 function InputSample() {
-    const [value, setValue] = useState('');
+  const [value, setValue] = useState('');
 
-    const onChange = (e) => {
-        setValue(value => e.target.value);
-    }
-    const onClick = () => {
-        setValue(value => '');
-    }
+  const onChange = (e) => {
+    setValue((value) => e.target.value);
+  };
+  const onClick = () => {
+    setValue((value) => '');
+  };
 
-    return (
-        <div>
-            {/* input 내 value = {value} => 버튼 클릭 시 input 값 초기화 */}
-            <input type="text" onChange={onChange} value={value}/>
-            <button onClick={onClick}>초기화</button>
-            <div>
-                <b>값: </b>
-                {value}
-            </div>
-        </div>
-    )
-} export default InputSample;
+  return (
+    <div>
+      {/* input 내 value = {value} => 버튼 클릭 시 input 값 초기화 */}
+      <input type="text" onChange={onChange} value={value} />
+      <button onClick={onClick}>초기화</button>
+      <div>
+        <b>값: </b>
+        {value}
+      </div>
+    </div>
+  );
+}
+export default InputSample;
