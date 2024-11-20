5046734c6e62a771db2acb34497a69cbb0e3d776
gsygh <gsygh@naver.com>
2024-11-20 21:31:12 +0900

    ~Scss

--- react_example/styling/src/components/Button.js
+++ react_example/styling/src/components/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
+import './Button.scss';
 
-function Button() {
-  return <div>'ds'</div>;
+function Button({ children }) {
+  return <button className="Button">{children}</button>>
 }
 export default Button;
